dba10c44b86099724521518d14f4fcabc9b54066
Charlie Bickerton <charlie@mimin.al>
2019-06-04 16:27:55 +0100

    webcam setup function

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const webcamElement = document.getElementById('webcam');
 
 let net;
 
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+      navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+      navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({ video: true },
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata', () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
+}
+
 async function app() {
   console.log('Loading mobilenet..');
 
