55ff6848c0991660246a977d78acfb1bc3b2fa04
Charlie Bickerton <charlie@mimin.al>
2019-06-04 16:28:49 +0100

    change prediction to be an infinate loop

--- index.js
+++ index.js
@@ -28,10 +28,19 @@ async function app() {
   net = await mobilenet.load();
   console.log('Sucessfully loaded model');
 
-  // Make a prediction through the model on our image.
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
-  console.log(result);
+  await setupWebcam();
+  while (true) {
+    const result = await net.classify(webcamElement);
+
+    document.getElementById('console').innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
+  }
 }
 
 app();
